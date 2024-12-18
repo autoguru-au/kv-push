d3eb216c174fd55c0ea676e789a6444afb60560e
vanguille <guillermofernandezcadarso@gmail.com>
2024-12-18 19:26:42 +0100

    test

---
+++ src/KeyValuePush.Tests/BasicTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace KeyValuePush.Tests
+{
+    public class BasicTests
+    {
+        [Fact]
+        public void ExampleTest()
+        {
+            Assert.True(1 + 1 == 2);
+        }
+    }
+}
